2376ee517c041b62ebd4d98e66a85a5e0b0f6b53
Jero <jeroci3lo@gmail.com>
2022-11-26 15:30:23 +0900

    First Upload

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: "class",
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        burtons: "burtons",
+        source_code_pro: "source_code_pro",
+      }
+    },
+  },
+  plugins: [],
+}
